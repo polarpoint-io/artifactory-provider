826e41a3280512e18b13790ffccae3668bd79efc
Surjit Bains <surjit.bains@newday.co.uk>
2024-12-09 16:37:55 +0000

    feat: initial commit

--- apis/v1beta1/register.go
+++ apis/v1beta1/register.go
@@ -13,7 +13,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "template.upbound.io"
+	Group   = "artifactory."
 	Version = "v1beta1"
 )
 
--- cmd/generator/main.go
+++ cmd/generator/main.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/crossplane/upjet/pkg/pipeline"
 
-	"github.com/upbound/upjet-provider-template/config"
+	"github.com/polarpoint-io/provider-artifactory/config"
 )
 
 func main() {
